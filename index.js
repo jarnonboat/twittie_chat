45a0608afd0dd86a377d64394686b42124d0c40a
Arnon <jarnonboat@gmail.com>
2016-04-13 14:14:49 +0700

    Pilot Lunch

---
+++ index.js
@@ -0,0 +1,282 @@
+var express = require("express");
+var mongoose = require('mongoose');
+var assert = require("assert");
+var favicon = require('serve-favicon');
+var autoIncrement = require('mongoose-auto-increment');
+var functions = require("./public/js/functions.js");
+
+var url = 'mongodb://localhost:27017/chat';
+var Schema = mongoose.Schema;
+var connection = mongoose.createConnection(url);
+autoIncrement.initialize(connection);
+
+// Defining model for mongodb
+var userSchema = new Schema({
+  user_id: Number,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  created_at: Date,
+  updated_at: Date
+});
+
+// Add the date before any save
+userSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
+var messageSchema = new Schema({
+  message_id: Number,
+  user_id: String,
+  user_name: String,
+  room_id: String,
+  message: String,
+  created_at: Date
+});
+
+// Add the date before any save
+messageSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
+var roomSchema = new Schema({
+  room_id: Number,
+  room_name: String,
+  user: String,
+  type: String,
+  created_at: Date
+});
+
+roomSchema.pre('save',function(next){
+  var currentDate = new Date();
+  if (!this.created_at)
+    this.created_at = currentDate;
+  next();
+})
+
+// Create User and Message schema
+userSchema.plugin(autoIncrement.plugin, {
+  model: 'User',
+  field: 'user_id',
+  startAt: 1,
+  incrementBy: 1
+});
+messageSchema.plugin(autoIncrement.plugin, {
+  model: 'Message',
+  field: 'message_id',
+  startAt: 1,
+  incrementBy: 1
+});
+roomSchema.plugin(autoIncrement.plugin, {
+  model: 'Room',
+  field: 'room_id',
+  startAt: 1,
+  incrementBy: 1
+});
+
+// Create User and Message schema
+var User = connection.model('User', userSchema);
+var Message = connection.model('Message', messageSchema);
+var Room = connection.model('Room',roomSchema);
+
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var port = 3700;
+var users = [];
+var userSockets = [];
+var rooms = [];
+var mainRoom = 'expresschat';
+
+// Setting template engine Jade
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+// Require public folder resources
+app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// Pass express to socket.io
+var io = require('socket.io').listen(server);
+
+// Initiate socket to handle all connection
+io.sockets.on('connection', function (socket) {
+	var _clientId = socket.id;
+
+  socket.join(mainRoom);
+
+  if (rooms.indexOf(mainRoom) == -1) {
+    rooms.push(mainRoom);
+    //rooms.push('dissys')
+  }
+
+  socket.on('addroom',function(roomId,currentId){
+    if(rooms.indexOf(roomId) == -1){
+      rooms.push(roomId);
+      //add to db;
+      var newroom = new Room({
+        room_name: roomId,
+        user: currentId,
+        type: "join"
+      });
+
+      newroom.save(function(err){
+        if(err != null){
+          console.log('There is an error creating room' + err);
+        }
+      });
+    }else{
+      socket.emit('exception', {message: 'This room is already existed'});
+    }
+    socket.broadcast.emit('updateroom',rooms);
+    socket.emit('updateroom',rooms);
+  });
+
+  // Load message for rooms
+  socket.on('load_message', function (_clientId, roomId) {
+    console.log('Loading message for room ' + roomId);
+    //var rooms = roomId.split('_');
+
+    // if (rooms.length == 2) {
+    //   var room2 = rooms[1] + '_' + rooms[0];
+    //   console.log('Load from ' + roomId + ' - ' + room2);
+    //   Message.find().or([{ room_id: roomId }, { room_id: room2}]).sort({'created_at': 'asc'}).exec(function (err, messages) {
+    //     socket.emit('display_message', _clientId, messages);
+    //   });
+    // } else {
+      Message.find({ room_id: roomId }).sort({'created_at': 'asc'}).exec(function (err, messages) {
+        socket.emit('display_message', _clientId, messages);
+      });
+  });
+
+  // Trigger on send event
+  socket.on('send', function (data) {
+    var _clientUser = functions.findByKey(users, 'client_id', _clientId);
+    var _clientUserId = _clientUser.user_id;
+
+    var message = new Message({
+      user_id: _clientUserId,
+      user_name: data.username,
+      room_id: data.room_id,
+      message: data.message
+    });
+    message.save(function (err) {
+      if (err != null) {
+        console.log('There is an error saving data ' + err);
+      }
+    });
+
+    io.sockets.in(data.room_id).emit('message', _clientUserId, _clientId, data);
+  });
+
+  socket.on('subscribe', function (_clientUserId, clientId, room_id) {
+    if (room_id != mainRoom) {
+      socket.join(room_id);
+      if (rooms.indexOf(room_id) == -1) {
+        // Create private chat between this socket and client
+        //userSockets[clientId].join(room_id);
+        rooms.push(room_id);
+
+      }
+      console.log(rooms);
+    }
+
+    // Create message content to hold between these two users
+    io.sockets.in(room_id).emit('subscribe', _clientId, room_id);
+  });
+
+  // Listen for regist action
+  socket.on('regist', function (data) {
+    User.findOne({ username: data.username }, function (err, user) {
+      if (user == null) {
+        var newUser = new User({
+          username: data.username,
+          password: data.password
+        });
+
+        // Save user to database
+        newUser.save(function (err) {
+          console.log(err);
+
+          if (err == null) {
+            // Make this user online
+            User.findOne({ username: data.username }, function (err, user) {
+              console.log('User ' + user.username + ' is online');
+
+              users.push({"client_id" : _clientId, "user_name" : data.username, "user_id": user.user_id});
+
+              userSockets[_clientId] = socket;
+
+              // Add new user to channel
+              io.sockets.emit('show_user', user.user_id, _clientId, users,rooms);
+            });
+          }
+        })
+      } else {
+        socket.emit('exception', {message: 'This user is already registered'});
+      }
+    });
+  });
+
+  // Login event
+  socket.on('login', function (data) {
+    User.findOne({ username: data.username }, function (err, user) {
+      if (user == null) {
+        socket.emit('exception', {message: 'This user is not exist. Please create your account !'});
+      } else {
+        User.findOne( { username: data.username, password: data.password }, function (err, user) {
+          if (user == null) {
+            socket.emit('exception', {message: 'Wrong password !'});
+          } else {
+            console.log('User ' + user.username + ' is online');
+            // Add new user to store
+            users.push({"client_id" : _clientId, "user_name" : data.username, "user_id": user.user_id});
+
+            userSockets[_clientId] = socket;
+
+            // Add new user to channel
+            io.sockets.emit('show_user', user.user_id, _clientId, users,rooms);
+          }
+        });
+      }
+    });
+  });
+
+  // Listen for disconnect event
+  socket.on('disconnect', function () {
+    // Update current users online
+    functions.removeObject(users, _clientId);
+
+    // Remove user from all client channel
+    io.sockets.emit('remove_user', _clientId, users);
+
+    console.log('User ' + _clientId + ' disconnected');
+  });
+});
+
+server.listen(port);
+console.log('Server started on port ' + port);
---
+++ public/js/chat.js
@@ -0,0 +1,405 @@
+/**
+* TODO: Stream simple music between clients
+*/
+
+// SERVER address and port
+var SERVER = 'http://localhost:3700';
+
+var MAIN_ROOM = 'expresschat';
+
+// Chat window is current window
+var chatWindow = window.self;
+
+$(window).load(function() {
+
+  // Add regular expression to check username valid
+  $.validator.addMethod("regex", function(value, element, regexpr) {
+    return regexpr.test(value);
+  }, "Please enter a valid username.");
+
+  // Current user name
+  var _username = null;
+
+  // Current user_id
+  var _userId = null;
+
+  // socket of current connection
+  var socket = io.connect(SERVER);
+
+  // Field of message
+  var field = $("#message");
+
+  // Current client_id
+  var clientId = null;
+
+  // All users connected
+  var users = [];
+
+  // Current room_id. Default is MAIN_ROOM
+  var currRoomId = MAIN_ROOM;
+
+  // First register user
+  var loginDialog = new BootstrapDialog.show({
+    title: 'Login',
+    closable: false,
+    message: '<form id="login_form">Login ID: <input type="text" name="username" class="form-control" id="username"><br>Login Password: <input type="password" name="password" class="form-control" id="password"></form>',
+    onshown: function(dialogRef) {
+      $('#username').focus();
+    },
+    onhidden: function(dialogRef){
+      $('#message').focus();
+    },
+    buttons: [
+    {
+      label: 'Sign In',
+      cssClass: 'btn-primary',
+      action: function(dialogRef) {
+        $('#login_form').validate({
+          debug: true,
+          rules: {
+            username: {
+              required: true,
+              regex: /^\S+$/ // Check has no whitespace
+            },
+            password: "required"
+          }
+        });
+
+        var username = $('#username').val();
+        var password = $('#password').val();
+
+        if ($('#login_form').valid()) {
+          _username = username;
+          // Login user
+          socket.emit('login', { username: username, password: password });
+          $('body').after('<div id="active_room" style="display:none;">' + MAIN_ROOM + '</div>');
+          $('#profile').text(username);
+        }
+      }
+    },
+    {
+      label: 'Create Account',
+      action: function(dialogRef) {
+        $('#login_form').validate({
+          debug: true,
+          rules: {
+            username: {
+              required: true,
+                regex: /^\S+$/ // Check has no whitespace
+            },
+            password: "required"
+          }
+        });
+
+        var username = $('#username').val();
+        var password = $('#password').val();
+
+        if ($('#login_form').valid()) {
+          _username = username;
+          // Register user
+          socket.emit('regist', { username: username, password: password });
+          $('body').after('<div id="active_room" style="display:none;">' + MAIN_ROOM + '</div>');
+          $('#profile').text(username);
+        }
+      }
+    }
+    ]
+  });
+
+  // On exception, show alert dialog and reset username
+  socket.on('exception', function (data) {
+    _username = null;
+
+    alert(data.message);
+  });
+
+  /** Trigger message event
+  * _clientUserId id of user on server database
+  * _clientId id of current user socket
+  * data hold message data
+  */
+  socket.on('message', function (_clientUserId, _clientId, data) {
+    console.log('Message on room ' + data.room_id);
+    //console.log(currRoomId);
+    var room_id = data.room_id;
+
+    //var tempRoom = room_id.split('_');
+    //var tempRoomId = tempRoom.length == 2 ? tempRoom[1] + '_' + tempRoom[0] : '';
+
+    if(data.message) {
+      //console.log(data.message);
+      var cls = 'row';
+      // Handle on destination client
+      if (_clientId != clientId) {
+        cls = 'row_other';
+      //  notifyMe(data);
+        // if not current room show unread แทน
+        // If not is MAIN_ROOM, show unread count message
+        // if(currRoomId != room_id){
+        //   var currUnread = $('#user-list li#'+room_id+' .unread').text();
+        //   currUnread++;
+        //   $('#user-list li#'+room_id+' .unread').text(currUnread).show();
+        // }
+
+        if (room_id == MAIN_ROOM) {
+          if (currRoomId != MAIN_ROOM) {
+            var currUnread = $('#user-list li#main_room .unread').text();
+            currUnread++;
+            $('#user-list li#main_room .unread').text(currUnread).show();
+          }
+        } else if (currRoomId != room_id) {
+          // Show unread count message on private chat
+          var currUnread = $('#user-list li#'+room_id+'_1 .unread').text();
+          currUnread++;
+          $('#user-list li#'+room_id+'_1 .unread').text(currUnread).show();
+       }
+      }
+
+      if (currRoomId == room_id) {
+        //console.log(room_id,currRoomId);
+        // Show message on screen
+        var date = new Date();
+        var html = '<div class="' + cls + '">' +
+        '<div class="r-message"><div class="username">' + data.username + '</div><div class="message">' + data.message + '</div>' +
+        '<div class="profile"><img src="/images/profile.jpg" class="img-rounded"></div></div>' +
+        '<div class="date">' + date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2) + '</div>' +
+        '</div>';
+        $('#conversation').append(html).scrollTop($('#conversation')[0].scrollHeight);
+      }
+    } else {
+      console.log("There is a problem:", data);
+    }
+  });
+
+  /** Show user after logged in successfully
+  * _clientUserId id of user on server database
+  * _clientId id of current user socket
+  * _users array of all connected users
+  */
+  socket.on('show_user', function (_clientUserId, _clientId, _users,_rooms) {
+    // Set clientId for the first time
+    if (!clientId) {
+      clientId = _clientId;
+      _userId = _clientUserId;
+    }
+
+    // Close login dialog
+    loginDialog.close();
+
+    // Main chat room
+    if (!$('#main_room').is(':visible')) {
+      var html = '<li class="row-user active" id="main_room" data-rid="' + MAIN_ROOM + '"><span class="user_name">Main Room</span><span class="unread">0</span></li>';
+      $('#user-list').append(html);
+    }
+    users = _users;
+    $.each(_rooms, function(key, value){
+      if(value != MAIN_ROOM){
+        var html = '<li class="row-user" id="'+value+'_1" data-rid="' + value + '"><span class="user_name">'+value+'</span><span class="unread">0</span></li>';
+        $('#user-list').append(html);
+      }
+    });
+
+    // Show all users. If new users connected, only show that user
+    // for (key in users) {
+    //   var user = users[key];
+    //
+    //   var cId = user.client_id;
+    //   var userId = user.user_id;
+    //   var username = user.user_name;
+    //
+    //   if (_username == username) {
+    //     continue;
+    //   }
+    //
+    //   // If this user is not shown, show it
+    //   if (!$('#' + cId).is(':visible')) {
+    //     var html = '<li class="row-user" id="' + cId + '" data-rid="' + userId + '"><img src="/images/profile.jpg" class="img-circle"><span class="user_name">' + username + '</span><span class="unread">0</span></li>';
+    //
+    //     $('#user-list').append(html);
+    //   }
+    // }
+
+    // Display message history
+    socket.emit('load_message', _clientId, MAIN_ROOM);
+  });
+
+  socket.on('updateroom',function(_rooms){
+    console.log("New Room added : " + _rooms);
+    $('#user-list').empty();
+		$.each(_rooms, function(key, value) {
+			if(value == MAIN_ROOM){
+        var html = '<li class="row-user" id="main_room" data-rid="' + MAIN_ROOM + '"><span class="user_name">Main Room</span><span class="unread">0</span></li>';
+        $('#user-list').append(html);
+			}
+			else {
+        var html = '<li class="row-user" id="'+value+'_1" data-rid="' + value + '"><span class="user_name">'+value+'</span><span class="unread">0</span></li>';
+        $('#user-list').append(html);
+			}
+		});
+    $('#user-list li[data-rid=' + currRoomId + ']').addClass('active');
+
+  });
+
+  /**
+  * _clientId id of current user socket
+  * room_id room_id that user want to connect to
+  */
+  socket.on('subscribe', function (_clientId, room_id) {
+    // Show messages of this room
+    if (_clientId == clientId) {
+      currRoomId = room_id;
+
+      console.log('Subscribe Room ' + currRoomId);
+      // Load messages for this room
+      socket.emit('load_message', _clientId, currRoomId);
+      $('#active_room').text(currRoomId);
+    }
+  });
+
+  // Remove users from data
+  socket.on('remove_user', function (_clientId, _users) {
+    users = _users;
+
+      // Remove from channel
+      $('#' + _clientId).remove();
+    });
+
+  socket.on('display_message', function (_clientId, messages) {
+    $('#conversation').html('');
+
+    for (key in messages) {
+      var message = messages[key];
+      var user_id = message.user_id;
+
+      var cls = 'row';
+      if (_userId != user_id) {
+        cls = 'row_other';
+      }
+
+      // Show message on screen
+      var date = new Date(message.created_at);
+      var today = new Date();
+      var dateString = '';
+      if (date.getDate() == today.getDate() && date.getMonth() == today.getMonth() && date.getFullYear() == today.getFullYear()) {
+        // Show only hour and minute
+        dateString = date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2);
+      } else {
+        if (date.getFullYear() == today.getFullYear()) {
+          dateString = (date.getMonth()+1) + '/' + date.getDate() + ' ' + date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2);
+        } else {
+          dateString = date.getFullYear() + '/' + (date.getMonth()+1) + '/' + date.getDate() + ' ' + date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2);
+        }
+      }
+      var html = '<div class="' + cls + '">' +
+      '<div class="r-message"><div class="username">' + message.user_name + '</div><div class="message">' + message.message + '</div>' +
+      '<div class="profile"><img src="/images/profile.jpg" class="img-rounded"></div></div>' +
+      '<div class="date">' + dateString + '</div>' +
+      '</div>';
+      $('#conversation').append(html).scrollTop($('#conversation')[0].scrollHeight);
+    }
+  });
+
+  /**
+  * User interaction. Active private chat for user clicked
+  */
+  $('#user').on('click', '.row-user', function () {
+    // Hide unread notify
+    $(this).find('.unread').text('').hide();
+
+    var roomId = $(this).attr('data-rid');
+    var _clientId = $(this).attr('id'); // Client ID of the socket connected
+    var roomTitle = $(this).find('.user_name').text();
+    //console.log(roomId,_clientId,roomTitle);
+    $('.room-title').text(roomTitle);
+
+    $('#user-list li').removeClass('active');
+
+    $('#user-list li[data-rid=' + roomId + ']').addClass('active');
+
+    var activeRoom = roomId;
+    // var activeRoomId;
+    // if(roomId == MAIN_ROOM){
+    //   activeRoomId = 'main_room';
+    // }else {
+    //   activeRoomId = roomId+'_1';
+    // }
+    // console.log(activeRoomId);
+    // console.log($('#'+activeRoomId).length);
+    if ($('#' + activeRoom).length == 0) {
+        // Change room for private chat
+        socket.emit('subscribe', _userId, _clientId, roomId);
+    } else {
+      // Only active current private chat
+      currRoomId = activeRoom;
+
+      // Load messages for this room
+      socket.emit('load_message', _clientId, currRoomId);
+      $('#active_room').text(currRoomId);
+    }
+
+    $('#message').focus();
+  });
+
+  $('#newroom').click(function(){
+    var roomname = $('#newroomname').val().trim();
+    //console.log(roomname);
+    if(roomname != ''){
+      socket.emit('addroom',roomname,_username);
+      $('#newroomname').val('');
+    }else{
+      alert('Please enter room name');
+    }
+  });
+
+  // User click Send button
+  $('#send').click(function() {
+    var text = field.val().trim();
+
+    if (text !== '') {
+      socket.emit('send', { message: text, username: _username, room_id: currRoomId });
+
+      field.val('').focus();
+    }
+  });
+
+  // Catch when user press Enter on keyboard
+  $('#message').keypress(function(e) {
+    var text = field.val().trim();
+
+    if (e.which == 13 && text !== '') {
+      socket.emit('send', { message: text, username: _username, room_id: currRoomId });
+
+      console.log('Send message in room ' + currRoomId);
+
+      $(this).val('').focus();
+    }
+  });
+});
+
+// Show desktop notification
+// $(function() {
+//   // request permission on page load
+//   if (Notification.permission !== "granted")
+//     Notification.requestPermission();
+// });
+//
+// function notifyMe(data) {
+//   if (!Notification) {
+//     alert('Desktop notifications not available in your browser. Try Chromium.');
+//     return;
+//   }
+//
+//   if (Notification.permission !== "granted")
+//     Notification.requestPermission();
+//   else {
+//     var notification = new Notification('New message', {
+//       icon: SERVER + '/images/so_icon.png',
+//       body: data.message,
+//     });
+//
+//     // Open and active current chat window
+//     notification.onclick = function () {
+//       chatWindow.focus();
+//     };
+//   }
+// }
